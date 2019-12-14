deb8ee4c81a2863f15b5b9dc76aed8d89e3f2f34
John Pavley <john@pavley.com>
2019-12-14 18:48:54 -0500

    Initial Commit

---
+++ Photo Fun/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Photo Fun
+//
+//  Created by John Pavley on 12/14/19.
+//  Copyright © 2019 John Pavley. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
